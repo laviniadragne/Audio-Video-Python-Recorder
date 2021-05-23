91c98cfcad4bae8f2d4f07fb3fc396a12786ecdf
laviniadragne <57103351+laviniadragne@users.noreply.github.com>
2021-05-23 20:21:17 +0300

    Add files via upload

--- audio.py
+++ audio.py
@@ -1,11 +1,22 @@
 import pyaudio
+import numpy as np
+import pyautogui
+from scipy.io.wavfile import read
+import matplotlib.pyplot as plt
+import math
+import statistics 
+import sys
 
-# p = pyaudio.PyAudio()
-# info = p.get_host_api_info_by_index(0)
-# numdevices = info.get('deviceCount')
-# for i in range(0, numdevices):
-#     if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-#         print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+def power(my_list):
+    return [ x**2 for x in my_list ]
 
-
-list_recording_device_index_names()
+filename = "recorded.wav"
+samprate, wavdata = read(filename)
+    # basically taking a reading every half a second - the size of the data 
+    # divided by the sample rate gives us 1 second chunks so I chop 
+    # sample rate in half for half second chunks
+chunks = np.array_split(wavdata, wavdata.size/(samprate/2))
+# for chu in chunks: 
+#     print((chu.flatten()).flatten())
+dbs = [20*math.log10(math.sqrt(statistics.mean(power(chu.flatten())))) for chu in chunks]
+print(dbs)
